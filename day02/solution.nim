7a813612ce3c5f97a6e7f1c392f144d2c714d0f4
nitely <ecastroborsani@gmail.com>
2023-12-02 19:44:14 -0300

    scanf only

--- day02/solution.nim
+++ day02/solution.nim
@@ -3,51 +3,47 @@ import std/strscans
 
 proc day2(): int =
   result = 0
-  let input = readFile("./input")
-  for line in input.splitLines:
-    if line.len == 0: continue
-    var game = 0
-    var rest = ""
-    let m = scanf(line, "Game $i: $*", game, rest)
-    doAssert m
+  var input = readFile("./input")
+  var game = 0
+  var gameSets = ""
+  while scanf(input, "Game $i: $*\n$*", game, gameSets, input):
     var isOk = true
     var num = 0
     var color = ""
-    while scanf(rest.strip(chars={' ', ',', ';'}), "$i $w$*", num, color, rest):
+    while scanf(gameSets.strip(chars={' ', ',', ';'}), "$i $w$*", num, color, gameSets):
       let isColorOk = case color:
         of "red": num <= 12
         of "green": num <= 13
         of "blue": num <= 14
         else: doAssert false; false
       isOk = isOk and isColorOk
-    doAssert rest.len == 0
+    doAssert gameSets.len == 0
     if isOk:
       result += game
+  doAssert input.len == 0
 
 echo day2()
 
 proc day2b(): int =
   result = 0
-  let input = readFile("./input")
-  for line in input.splitLines:
-    if line.len == 0: continue
-    var game = 0
-    var rest = ""
-    let m = scanf(line, "Game $i: $*", game, rest)
-    doAssert m
+  var input = readFile("./input")
+  var game = 0
+  var gameSets = ""
+  while scanf(input, "Game $i: $*\n$*", game, gameSets, input):
     var
       red = 0
       green = 0
       blue = 0
     var num = 0
     var color = ""
-    while scanf(rest.strip(chars={' ', ',', ';'}), "$i $w$*", num, color, rest):
+    while scanf(gameSets.strip(chars={' ', ',', ';'}), "$i $w$*", num, color, gameSets):
       case color:
         of "red": red = max(red, num)
         of "green": green = max(green, num)
         of "blue": blue = max(blue, num)
         else: doAssert false
-    doAssert rest.len == 0
+    doAssert gameSets.len == 0
     result += red * green * blue
+  doAssert input.len == 0
 
 echo day2b()
